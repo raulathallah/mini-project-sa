8bcf684a51eb995cc40d9b0fed983621b055f259
raulathallah <raultaqi@gmail.com>
2024-12-11 15:02:45 +0700

    merge

--- Asg8.LibraryManagementSystem.WebApi/LibraryManagementSystem.Domain/obj/Debug/net8.0/LibraryManagementSystem.Domain.AssemblyInfo.cs
+++ Asg8.LibraryManagementSystem.WebApi/LibraryManagementSystem.Domain/obj/Debug/net8.0/LibraryManagementSystem.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("LibraryManagementSystem.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7c30e8a35814da9460387d7496917689a42ae4d2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+89244e70bc31027a9c74de9e95af20a7aefc31ed")]
 [assembly: System.Reflection.AssemblyProductAttribute("LibraryManagementSystem.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("LibraryManagementSystem.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
